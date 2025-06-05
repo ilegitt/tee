94eb09c9e2210e1ae069ff5d1a2019fcacb2b056
Bakare Bakare <bakareandbakare@gmail.com>
2025-06-05 17:57:44 +0200

    tee-project2

--- main.tf
+++ main.tf
@@ -129,10 +129,18 @@ module "eks_control_plane" {
 ############################
 data "aws_eks_cluster" "cluster" {
   name = module.eks_control_plane.cluster_name
+
+  depends_on = [
+    module.eks_control_plane
+  ]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks_control_plane.cluster_name
+
+  depends_on = [
+    module.eks_control_plane
+  ]
 }
 
 ############################
